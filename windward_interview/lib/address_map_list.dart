b1f81a87894692090d43537c5cbe5e35288ff210
Tom Frawley <t.c.frawley1012@gmail.com>
2024-10-30 13:58:44 -0500

    Added Task List

---
+++ windward_interview/lib/address_map_list.dart
@@ -0,0 +1,108 @@
+
+List<Map<String, String>> addresses = [
+    {
+      "street": "123 Main St",
+      "city": "Springfield",
+      "state": "IL",
+      "zipCode": "62704",
+      "country": "USA"
+    },
+    {
+      "street": "456 Elm St",
+      "city": "Metropolis",
+      "state": "NY",
+      "zipCode": "10001",
+      "country": "USA"
+    },
+    {
+      "street": "789 Oak Ave",
+      "city": "Gotham",
+      "state": "NJ",
+      "zipCode": "07001",
+      "country": "USA"
+    },
+    {
+      "street": "101 Maple Dr",
+      "city": "Smallville",
+      "state": "KS",
+      "zipCode": "66002",
+      "country": "USA"
+    },
+    {
+      "street": "202 Pine Ln",
+      "city": "Central City",
+      "state": "MO",
+      "zipCode": "64030",
+      "country": "USA"
+    },
+    {
+      "street": "303 Cedar Blvd",
+      "city": "Star City",
+      "state": "CA",
+      "zipCode": "94016",
+      "country": "USA"
+    },
+    {
+      "street": "404 Birch St",
+      "city": "Coast City",
+      "state": "FL",
+      "zipCode": "32901",
+      "country": "USA"
+    },
+    {
+      "street": "505 Spruce St",
+      "city": "Blüdhaven",
+      "state": "PA",
+      "zipCode": "19012",
+      "country": "USA"
+    },
+    {
+      "street": "606 Redwood Rd",
+      "city": "Keystone City",
+      "state": "OH",
+      "zipCode": "43001",
+      "country": "USA"
+    },
+    {
+      "street": "707 Walnut St",
+      "city": "Riverdale",
+      "state": "CO",
+      "zipCode": "80002",
+      "country": "USA"
+    },
+    {
+      "street": "808 Chestnut Dr",
+      "city": "Duckburg",
+      "state": "TX",
+      "zipCode": "75001",
+      "country": "USA"
+    },
+    {
+      "street": "909 Aspen Ave",
+      "city": "Arlen",
+      "state": "TX",
+      "zipCode": "75501",
+      "country": "USA"
+    },
+    {
+      "street": "1010 Palm St",
+      "city": "Bedrock",
+      "state": "AZ",
+      "zipCode": "86001",
+      "country": "USA"
+    },
+    {
+      "street": "1111 Fir St",
+      "city": "South Park",
+      "state": "CO",
+      "zipCode": "80440",
+      "country": "USA"
+    },
+    {
+      "street": "1212 Cypress St",
+      "city": "Hill Valley",
+      "state": "CA",
+      "zipCode": "95420",
+      "country": "USA"
+    }
+  ];
